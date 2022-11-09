a99165559f1f32941dafd6c41c8d997e659cc7f0
ahsan sayeed <ahsansayeedsaad@gmail.com>
2022-11-09 11:28:11 +0000

    router modify

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -17,6 +17,7 @@ async function run() {
   try {
     const database = client.db("Wedding");
     const collection = database.collection("Services");
+    const reviewCollection = database.collection("Reviews")
 
     app.post('/services',async(req,res)=>{
         const {title,email,price,imageUrl,shortDesc,fullDesc} = req.body;
@@ -25,13 +26,37 @@ async function run() {
         console.log(result);
         res.status(200).send(result);
     })
-    app.get('/services/:limit',async(req,res)=>{
-        const limit = parseInt(req.params.limit);
+    app.get('/services',async(req,res)=>{
+        const limit = parseInt(req.query.limit);
+        const skip = parseInt(req.query.skip);
         const query = {};
         const options = {};
         const cursor = collection.find(query,options);
-        const result = await cursor.limit(limit).toArray();
-        res.status(200).send(result);
+        const result = await cursor.skip(skip).limit(limit).toArray();
+        const count = await collection.estimatedDocumentCount();
+        res.status(200).send({count,result});
+    })
+    app.get('/services/:id',async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id:ObjectId(id)};
+      const options = {};
+      const result = await collection.findOne(query,options);
+      res.status(200).send(result);
+    })
+    app.post('/review',async(req,res)=>{
+      const {email,imageUrl,message,rating,displayName} = req.body;
+      const date = (new Date(Date.now())).toDateString() + " " + (new Date(Date.now())).toLocaleTimeString();
+      const doc = {email,displayName,message,rating,imageUrl,date};
+      const result = await reviewCollection.insertOne(doc);
+      res.status(200).send(result);
+    })
+    app.get('/review',async(req,res)=>{
+      const query = {};
+      const options = {};
+      const cursor = reviewCollection.find(query,options);
+      const result = await cursor.toArray();
+      const count = await reviewCollection.estimatedDocumentCount();
+      res.status(200).send({count,result});
     })
 
   } finally {
