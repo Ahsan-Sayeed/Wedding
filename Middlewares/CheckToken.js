225804422479a1cb5d1a1f7fa785d0391008d1a6
ahsan sayeed <ahsansayeedsaad@gmail.com>
2022-11-10 16:27:54 +0000

    bug fixed

--- Middlewares/CheckToken.js
+++ Middlewares/CheckToken.js
@@ -2,10 +2,11 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 exports.verifyToken = (req,res,next) =>{
     const auth = req.headers.authorization;
+
     if(!auth){
        return res.status(401).send({message:"unauthorized"})
     }
-    jwt.verify(req.query.Token, process.env.SECRET_TOKEN,(err,decoded)=>{
+    jwt.verify(auth.split(" ")[1], process.env.SECRET_TOKEN ,(err,decoded)=>{
         if(err){
             res.status(401).send({message:"unauthorized"})
         }
